47a0ce50d3fea2e09badc6ea0dcb287c499660d2
Hayden Fortunata <hayden.fortunata@axiomteksystems.local>
2026-01-13 15:37:16 -0500

    started search by keyworad

--- headers/search_record.c
+++ headers/search_record.c
@@ -12,6 +12,37 @@ void clear_buffer(){
     while ((ch = getchar()) != '\n' && ch != EOF);
 }
 
+void search_record_byKeyword( char *csvFile){
+    int rows = count_rows(csvFile);
+    FILE *file = fopen( csvFile , "r");
+    char buffer[1024];
+    int arrayCount = 0;
+    while(1){
+        clear_buffer();
+        printf("Enter the keyword you would like to search , type 0 if you would like to escape this option: ");
+        char input[100];
+        
+        if(fgets( input , sizeof(input) , stdin)){
+            // strip the new character
+            input[strcspn(input, "\n")] = '\0';
+            // quit if pressed 0
+            if(strcmp(input , "0") == 0){
+                clear_screen();
+                return;
+            }
+            fgets( buffer , sizeof(buffer) , file); //  read the header and do nothing
+            char **lines = malloc((size_t)rows * sizeof *lines);
+
+            // add the strings to an array
+            while((fgets( buffer , sizeof(buffer) , file))){
+                lines[arrayCount] = malloc(strlen(buffer) + 1);
+                strcpy(lines[arrayCount] , buffer);
+                arrayCount++;
+            }
+        }
+    }
+}
+
 void print_found_record( char *string){
     clear_screen();
     printf("StudentID Name   BookID    Book Name\n\n");
@@ -23,7 +54,7 @@ void print_found_record( char *string){
     clear_screen();
 }
 
-void search_record( char *csvFile){
+void search_record_byID( char *csvFile){
     while(1){
         printf("Enter the ID you would like to search , type 0 if you would like to escape this option: ");
         int searchNum = verify_choice();
@@ -59,9 +90,10 @@ void search_record( char *csvFile){
 }
 
 int search_menu(){
-    printf("1) Would you like to search a record?\n");
-    printf("2) Or Go Back to the Main Menu\n");
-    printf("Please select from 1 or 2: ");
+    printf("1) Would you like to search a record by student ID?\n");
+    printf("2) Would you like to search a record by keyword\n");
+    printf("3) Or Go Back to the Main Menu\n");
+    printf("Please select from 1 or 3: ");
     int num = verify_choice();
     return num;
 }
@@ -71,16 +103,19 @@ void search_record_main( char *csvFile ){
     printf("\n-------------Search Records-------------\n");
     while(1){
         int searchMenuOption = search_menu();
-        if ( searchMenuOption >= 3 || searchMenuOption < 1){
+        if ( searchMenuOption >= 4 || searchMenuOption < 1){
             printf("Invalid, please enter a number between 1-2\n");
             continue;
         } // if (menuOption == -1)
 
         switch(searchMenuOption){
             case 1:
-                search_record( csvFile );
+                search_record_byID( csvFile );
                 break;
             case 2:
+                search_record_byKeyword( csvFile );
+                break;
+            case 3:
                 clear_screen();
                 printf("\n-------------Library Books Management System-------------\n");
                 return;
